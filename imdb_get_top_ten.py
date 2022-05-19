7a18e6e970a4ea92b8ded9d2dbe49d1c8fbe99f4
colinZejda <65974025+colinZejda@users.noreply.github.com>
2022-05-18 22:58:31 -0700

    Update imdb_get_top_ten.py

--- imdb_get_top_ten.py
+++ imdb_get_top_ten.py
@@ -4,9 +4,10 @@ def get_top_10_imdb():
     lst = []
     ia = imdb.IMDb()
     search = ia.get_top250_movies()
-    for i in range(10):
+    for i in range(15):
         lst.append(search[i])
     return lst
 
-#for i in get_top_10_imdb():
-#    print(i)
+if __name__ == '__main__':
+    for i in get_top_10_imdb():
+        print(i)
